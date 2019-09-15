73bf9e4783b07c8ea6954f4541e6e14dcc99ddf1
SLowrance <steve.lowrance@aronsonsecurity.com>
2019-09-14 20:30:29 -0700

    small outline of new features

--- game.py
+++ game.py
@@ -2,3 +2,11 @@ class Game:
     def __init__(self):
         self.turns = 0
         self.kills = 0
+        self.game_end = False
+
+    def start_game(self):
+        while not self.game_end:
+            self.get_action()
+
+    def get_action(self, location):
+        pass
--- tests/test_game.py
+++ tests/test_game.py
@@ -8,3 +8,15 @@ def test_new_game():
     assert game.turns > 0
     game = Game()
     assert game.turns == 0
+
+def test_look_around_event():
+    pass
+
+def test_look_around_opponent():
+    pass
+
+def test_rest_safely():
+    pass
+
+def test_rest_danger():
+    pass
